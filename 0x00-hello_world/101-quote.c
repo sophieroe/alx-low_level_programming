b86ac2cd97776cb7b530d2d1725bfcc14ccb236d
sophieroe <roseticia344@gmail.com>
2022-03-10 13:10:40 -0800

    file edited

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,11 @@
-#include <unistd.h>
+#!/bin/bash
 /**
-
-*main-prints exactly "and that piece of art is useful"
-
-*-Sophie Rose,2022-03-10",
-
-*followed by a new line,to the standard error.
-
-*Retun:1 if success.
-
-*/
-
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
-
 {
-
-write(2,"and that piece of art is useful\-Dora Korpar,2015-10-19\n",59);
-
-return(1);
-
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
