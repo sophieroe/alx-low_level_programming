f57362dadab0571c800655fb6dfeaa3962c2c1c9
sophieroe <roseticia344@gmail.com>
2022-03-16 23:41:44 +0300

    file added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Sec
