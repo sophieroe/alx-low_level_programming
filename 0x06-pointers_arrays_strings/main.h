07c59b0ed93572df6fdd53263c3a31a80f31c7c5
sophieroe <roseticia344@gmail.com>
2022-03-25 20:15:40 +0300

    file added

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ char *string_toupper(char *);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
+void print_number(int n);
 
 #endif /* MAIN_H */
