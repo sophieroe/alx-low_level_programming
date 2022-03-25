5ff8326165b47d3b481b0259bc0b20b067cb91a3
sophieroe <roseticia344@gmail.com>
2022-03-25 20:14:14 +0300

    file added

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
 
 #endif /* MAIN_H */
