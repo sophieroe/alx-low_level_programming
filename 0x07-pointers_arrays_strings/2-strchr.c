4ed5e59edf0ae90305f4893877c415e4d4a87a38
sophieroe <roseticia344@gmail.com>
2022-03-28 21:59:13 +0300

    file added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
 char *_strchr(char *s, char c)
 {
-	for (; *s != '\0' && *s != c; ++s)
-		;
-	return (*s == c ? (char *) s : NULL);
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
 }
