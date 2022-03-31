9ca98cbe9f181d0acfd485cf1a671c1796f36e0d
sophieroe <roseticia344@gmail.com>
2022-03-31 21:46:52 +0300

    file edited

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,87 +1,56 @@
-int _strlen_recursion(char *s)
-{
+#include "main.h"
 
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
-}
+int check_palindrome(char *s);
 
 /**
-* palidrone - function
-*
-* @s: chaine of the char
-* @taille: the number
-* @i: the number
-* Return: Always 0.
-*/
-int palidrone(char *s, int taille, int i)
-{
-	if (s[i] != '\0')
-	{
-		if (s[i] != s[taille - i])
-			return (0);
-		else
-			return (palidrone(s, taille, i + 1));
-	}
-	else
-		return (1);
-}
-
-/**
-* is_palindrome - function
-*
-* @s: the chaine of char
-* Return: Always 0.
-*/
-
+  * is_palindrome - Returns if a string is palindrome
+  * @s: the string value to be checked
+  *
+  * Return: integer value
+  */
 int is_palindrome(char *s)
 {
-	if (s[0] == '\0')
+	if (*s == '0')
 		return (1);
-	return (palidrone(s, _strlen_recursion(s) - 1, 0));
-}
-int _strlen_recursion(char *s)
-{
 
-	if (*s == '\0')
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	return (check_palindrome(s));
 }
 
 /**
-* palidrone - function
-*
-* @s: chaine of the char
-* @taille: the number
-* @i: the number
-* Return: Always 0.
-*/
-int palidrone(char *s, int taille, int i)
+  * check_palindrome - Check if a string is palindrome
+  * @s: the string value to be checked
+  *
+  * Return: integer value
+  */
+int check_palindrome(char *s)
 {
-	if (s[i] != '\0')
+	int l = _strlen_recursion(s) - 1;
+
+	if (*s == s[l])
 	{
-		if (s[i] != s[taille - i])
-			return (0);
-		else
-			return (palidrone(s, taille, i + 1));
+		s++;
+		l--;
 	}
 	else
-		return (1);
+	{
+		return (0);
+	}
+return (1);
 }
 
 /**
-* is_palindrome - function
-*
-* @s: the chaine of char
-* Return: Always 0.
-*/
-
-int is_palindrome(char *s)
+  * _strlen_recursion - Get the length of a string
+  * @s: the string to get the length
+  *
+  * Return: the string length
+  */
+int _strlen_recursion(char *s)
 {
-	if (s[0] == '\0')
-		return (1);
-	return (palidrone(s, _strlen_recursion(s) - 1, 0));
-}
+	if (*s == '\0')
+	{
+		return (0);
+	}
 
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
