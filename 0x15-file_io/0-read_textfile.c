ee083925e5afed4d6747086ecf9b546f67f68c32
sophieroe <roseticia344@gmail.com>
2022-05-04 23:35:10 +0300

    file edited

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to text in a file
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
  */
-int main(int ac, char **av)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t n;
+    ssize_t file, fread, fwrite;
+    char *totalSize;
 
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 114);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+    totalSize = malloc(sizeof(char) * letters);
+    if (totalSize == NULL)
+        return (0);
+    if (filename == NULL)
+        return (0);
+
+    file = open(filename, O_RDONLY);
+    if (file == -1)
+        return (0);
+    fread = read(file, totalSize, letters);
+    if (fread == -1)
+        return (0);
+    fwrite = write(STDOUT_FILENO, totalSize, fread);
+    if (fwrite == -1)
+        return (0);
+    close(file);
+    free(totalSize);
+    return (fwrite);
 }
